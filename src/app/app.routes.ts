960755e5a52b9d296d04d2e65e04f266b36788eb
maryam mrg <maryamelouadaa@gmail.com>
2025-05-01 23:37:17 +0100

    creating signup component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
     { path : '',component : HomeComponent,},
     { path:'recipes',component: RecipeListComponent,},
     { path:'details', component: RecipeDetailComponent,},
-    {path:'login', component: LoginComponent,}
+    {path:'login', component: LoginComponent,},
+    {path : 'signup', component:SignupComponent}
+
 ];
